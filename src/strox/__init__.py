bea318e3944890cfccf77f4a53cc36d865f50cdf
Havsalt <77575424+Havsalt@users.noreply.github.com>
2025-12-28 14:24:17 +0100

    Added budget params to parser

--- src/strox/__init__.py
+++ src/strox/__init__.py
@@ -117,11 +117,63 @@ def main() -> int:
     class ParserArguments(argparse.Namespace):
         query: str
         options: list[str]
+        substitution_cost: float
+        insertion_cost: float
+        deletion_cost: float
+        equality_bonus: float
+        start_bonus: float
+        end_bonus: float
 
     parser = argparse.ArgumentParser(
         prog="strox",
         description="Command line interface for the `strox` package",
         add_help=False,
+    )
+    parser.add_argument(
+        "--substitution-cost",
+        "-s",
+        "--sub",
+        default=Budget._field_defaults["substitution_cost"],
+        help="Substitution cost",
+        metavar="FLOAT",
+    )
+    parser.add_argument(
+        "--insertion-cost",
+        "-i",
+        "--ins",
+        default=Budget._field_defaults["insertion_cost"],
+        help="Insertion cost",
+        metavar="FLOAT",
+    )
+    parser.add_argument(
+        "--deletion-cost",
+        "-d",
+        "--del",
+        default=Budget._field_defaults["deletion_cost"],
+        help="Deletion cost",
+        metavar="FLOAT",
+    )
+    parser.add_argument(
+        "--equality-bonus",
+        "-e",
+        "--eq",
+        default=Budget._field_defaults["equality_bonus"],
+        help="Equality bonus",
+        metavar="FLOAT",
+    )
+    parser.add_argument(
+        "--start-bonus",
+        "--sb",
+        default=Budget._field_defaults["start_bonus"],
+        help="Start bonus",
+        metavar="FLOAT",
+    )
+    parser.add_argument(
+        "--end-bonus",
+        "--eb",
+        default=Budget._field_defaults["end_bonus"],
+        help="End bonus",
+        metavar="FLOAT",
     )
     parser.add_argument(
         "-h",
@@ -153,6 +205,14 @@ def main() -> int:
     result = get_closest_match(
         args.query,
         args.options,
+        budget=Budget(
+            substitution_cost=args.substitution_cost,
+            insertion_cost=args.insertion_cost,
+            deletion_cost=args.deletion_cost,
+            equality_bonus=args.equality_bonus,
+            start_bonus=args.start_bonus,
+            end_bonus=args.end_bonus,
+        ),
     )
     sys.stdout.write(result)
 
