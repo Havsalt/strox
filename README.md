30ded1b878d38cc4b138c970094b8da815ed0806
Havsalt <77575424+Havsalt@users.noreply.github.com>
2025-12-28 21:25:52 +0100

    Added cli-color

--- README.md
+++ README.md
@@ -6,7 +6,15 @@ Find strings that matches approximately the given query.
 
 You can run `strox` from the terminal if installed, usually through `pipx install` or `uv tool install`.
 
-Example of querying for the closest matching option, with _weight_ parameter.
+To enable colors, install with the `cli-color` extra:
+
+```bash
+pipx install strox[cli-extra]
+# or
+uv tool install strox[cli-extra]
+```
+
+Example of querying for the closest matching option, with _weight_ parameter:
 
 ```bash
 strox "an" "Apple pie" "Indigo" "Banana" --insertion-cost 0.1
--- src/strox/_cli.py
+++ src/strox/_cli.py
@@ -1,8 +1,31 @@
-import argparse
+from __future__ import annotations
+
 import sys
+import argparse
+
+# Only *one* symbol will be available at runtime, hence the ignore
+from argparse import ArgumentDefaultsHelpFormatter as Formatter  # type: ignore
 from importlib.metadata import version
 
 from . import Budget, get_closest_match
+
+try:
+    from rich_argparse import ArgumentDefaultsRichHelpFormatter
+
+    class Formatter(ArgumentDefaultsRichHelpFormatter):
+        styles = {
+            "argparse.args": "bright_cyan",
+            "argparse.groups": "bold underline bright_yellow",
+            "argparse.help": "default",
+            "argparse.metavar": "bold white",
+            "argparse.syntax": "bold",
+            "argparse.text": "bright_white",
+            "argparse.prog": "bright_cyan",
+            "argparse.default": "italic magenta",
+        }
+
+except ModuleNotFoundError:
+    pass  # Using fallback symbol that is already imported as `Formatter`
 
 
 class ParserArguments(argparse.Namespace):
@@ -18,61 +41,62 @@ class ParserArguments(argparse.Namespace):
 
 def main() -> int:
     parser = argparse.ArgumentParser(
-        prog="strox",
+        prog="strox.exe",
         description="Command line interface for the `strox` package",
         add_help=False,
+        formatter_class=Formatter,
     )
     parser.add_argument(
         "--substitution-cost",
         "-s",
         "--sub",
         type=float,
+        metavar="FLOAT",
         default=Budget._field_defaults["substitution_cost"],
         help="Substitution cost",
-        metavar="FLOAT",
     )
     parser.add_argument(
         "--insertion-cost",
         "-i",
         "--ins",
         type=float,
+        metavar="FLOAT",
         default=Budget._field_defaults["insertion_cost"],
         help="Insertion cost",
-        metavar="FLOAT",
     )
     parser.add_argument(
         "--deletion-cost",
         "-d",
         "--del",
         type=float,
+        metavar="FLOAT",
         default=Budget._field_defaults["deletion_cost"],
         help="Deletion cost",
-        metavar="FLOAT",
     )
     parser.add_argument(
         "--equality-bonus",
         "-e",
         "--eq",
         type=float,
+        metavar="FLOAT",
         default=Budget._field_defaults["equality_bonus"],
         help="Equality bonus",
-        metavar="FLOAT",
     )
     parser.add_argument(
         "--start-bonus",
         "--sb",
         type=float,
+        metavar="FLOAT",
         default=Budget._field_defaults["start_bonus"],
         help="Start bonus",
-        metavar="FLOAT",
     )
     parser.add_argument(
         "--end-bonus",
         "--eb",
         type=float,
+        metavar="FLOAT",
         default=Budget._field_defaults["end_bonus"],
         help="End bonus",
-        metavar="FLOAT",
     )
     parser.add_argument(
         "-h",
@@ -90,12 +114,16 @@ def main() -> int:
     )
     parser.add_argument(
         "query",
+        type=str,
+        metavar="<query>",
         help="Query to match against",
     )
     parser.add_argument(
         "options",
         nargs="+",
-        help="Options to select from",
+        type=list[str],
+        metavar="<option>",
+        help="Options to run query against",
     )
 
     args = ParserArguments()
